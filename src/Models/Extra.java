1e9b5d0ff5eb3dea83ef5e6e835e3c7be2659cee
Jabbar <youssef.jabbar@capgemini.com>
2018-09-18 14:35:46 +0200

    Added getters and setters

--- src/Models/Extra.java
+++ src/Models/Extra.java
@@ -1,4 +1,24 @@
 package Models;
 
 public class Extra {
+
+
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/Models/Order.java
+++ src/Models/Order.java
@@ -1,4 +1,52 @@
 package Models;
 
+import java.sql.Time;
+
 public class Order {
+
+    private int orderId;
+    private int menuItem;
+    private int tableNumber;
+    private String orderStatus;
+    private Time time;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(int menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
