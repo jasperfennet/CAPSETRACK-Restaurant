df50fa5ae39ec38439463c45e6def0fb6249bf4e
remcodekoeijer <remcodekoeijer@hotmail.com>
2018-09-20 14:01:15 +0200

    Fixed mergeconflict

--- src/Tests/MenuItemTest.java
+++ src/Tests/MenuItemTest.java
@@ -1,6 +1,5 @@
 package Tests;
 
-<<<<<<< HEAD
 import Models.Address;
 import Models.MenuItem;
 import Models.Ingredient;
@@ -11,26 +10,19 @@ import org.junit.jupiter.api.Test;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-=======
-import Models.MenuItem;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
->>>>>>> 7067be5fc779f42f74f7babfb98f71f1d8625cdc
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuItemTest {
 
     private MenuItem menuItem;
     private ArrayList<Ingredient> ingredients;
-<<<<<<< HEAD
     private Supplier supplier;
     private Address address;
     private Ingredient ingr1, ingr2;
     private String pathToImage;
 
     @BeforeEach
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         address = new Address("Streetname", 155, "", "541000", "Wuhan");
         supplier = new Supplier("MeatDelivery", address, "0612345678", "NL12BANK1234567810");
 
@@ -45,30 +37,17 @@ class MenuItemTest {
 
         pathToImage = "src\\Images\\test.jpg";
     }
+
     @Test
     void getImage() {
         BufferedImage img = menuItem.setImage(pathToImage);
         assertEquals(img, menuItem.getImage());
-=======
 
-    @BeforeEach
-    public void setUp() throws Exception{
-        ingredients = new ArrayList<Ingredient>();
-    }
-    @Test
-    void getImage() {
-
->>>>>>> 7067be5fc779f42f74f7babfb98f71f1d8625cdc
     }
 
     @Test
     void setImage() {
-<<<<<<< HEAD
         assertNotNull(menuItem.setImage(pathToImage));
-=======
-        String pathToTest = "src\\Images\\test.jpg";
-        assertNotNull(menuItem.setImage(pathToTest));
->>>>>>> 7067be5fc779f42f74f7babfb98f71f1d8625cdc
     }
 
     @Test
