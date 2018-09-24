c406e2766fe7c418f1ab4a5e34635959fd8f2814
remcodekoeijer <remcodekoeijer@hotmail.com>
2018-09-24 15:42:47 +0200

    Added a tablestatus enum and changed the table with getters setters for that

--- src/Models/Table.java
+++ src/Models/Table.java
@@ -3,16 +3,17 @@ package Models;
 public abstract class Table {
 
     private int number, capacity;
+    private TableStatus status;
 
     public Table(int number, int capacity) {
         this.number = number;
         this.capacity = capacity;
+        status = TableStatus.AVAILABLE;
     }
 
     public int getNumber() {
         return this.number;
     }
-
     public void setNumber(int number) {
         this.number = number;
     }
@@ -20,8 +21,14 @@ public abstract class Table {
     public int getCapacity() {
         return this.capacity;
     }
-
     public void setCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public TableStatus getStatus(){
+        return this.status;
+    }
+    public void changeStatus(TableStatus status){
+        this.status = status;
     }
 }
