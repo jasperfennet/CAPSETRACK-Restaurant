a2abb7fb51a0115893da619db1da9324ff70e5ff
yjabbar <youssef.jabbar@capgemini.com>
2018-09-26 16:25:56 +0200

    TabeleType enum toegevoegd, table aangepast en TableTest op 100 procent coverage

--- src/MockData/InitializeData.java
+++ src/MockData/InitializeData.java
@@ -1,79 +1,79 @@
-package MockData;
-
-import Models.*;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Random;
-
-public class InitializeData {
-
-    private ArrayList<Guest> guests = new ArrayList<>();
-    private ArrayList<Booking> bookings = new ArrayList<>();
-    private ArrayList<Table> tables = new ArrayList<>();
-    private ArrayList<Order> orders = new ArrayList<>();
-    private ArrayList<MenuItem> menuItems = new ArrayList<>();
-    private ArrayList<Ingredient> ingredients = new ArrayList<>();
-    private ArrayList<Extra> extras = new ArrayList<>();
-
-
-    public InitializeData() {
-        for (int i = 0; i < 100; i++) {
-            guests.add(new Guest("" + 'a' + i,
-                    "" + 'b' + i,
-                    "" + ('a' + i) + "@" + ('b' + i) + ".com",
-                    "" + ('a' + i) + ('b' + i) + "street",
-                    "" + i,
-                    "" + ('a' + i) + ('b' + i) + ('c' + i)));
-            if(i < 50) {
-                tables.add(new RoundTable());
-            }else{
-                tables.add(new SquareTable());
-            }
-            extras.add(new Extra(""+i, i*(new Random().nextInt(5))));
-            ArrayList<Extra> tempExtras = new ArrayList<>();tempExtras.add(extras.get(i));
-            Address address = new Address("" + 'a' + i,
-                    i,
-                    "" + ('a' + i) + "@" + ('b' + i) + ".com",
-                    "" + ('a' + i) + ('b' + i) + "street",
-                    "" + i);
-            Supplier supplier = new Supplier("" + ('a' + i),
-                    address,
-                    ""+i + "" + (i+1) + "" + (i+2),
-                    ""+ i + "" + (i+1) + "" + (i+2));
-            ingredients.add(new Ingredient(""+('a' + i),"gram", i,i, supplier, "" + ('c' + i)));
-
-            menuItems.add(new MenuItem(ingredients));
-            orders.add(new Order(i,menuItems,i,"in progress", new Date()));
-            bookings.add(new Booking(guests.get(i),i,new Date(),new Random().nextInt(6),tempExtras, orders));
-        }
-    }
-
-    public ArrayList<Guest> getGuests() {
-        return guests;
-    }
-
-    public ArrayList<Booking> getBookings() {
-        return bookings;
-    }
-
-    public ArrayList<Table> getTables() {
-        return tables;
-    }
-
-    public ArrayList<Order> getOrders() {
-        return orders;
-    }
-
-    public ArrayList<MenuItem> getMenuItems() {
-        return menuItems;
-    }
-
-    public ArrayList<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public ArrayList<Extra> getExtras() {
-        return extras;
-    }
-}
+//package MockData;
+//
+//import Models.*;
+//
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.Random;
+//
+//public class InitializeData {
+//
+//    private ArrayList<Guest> guests = new ArrayList<>();
+//    private ArrayList<Booking> bookings = new ArrayList<>();
+//    private ArrayList<Table> tables = new ArrayList<>();
+//    private ArrayList<Order> orders = new ArrayList<>();
+//    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+//    private ArrayList<Ingredient> ingredients = new ArrayList<>();
+//    private ArrayList<Extra> extras = new ArrayList<>();
+//
+//
+//    public InitializeData() {
+//        for (int i = 0; i < 100; i++) {
+//            guests.add(new Guest("" + 'a' + i,
+//                    "" + 'b' + i,
+//                    "" + ('a' + i) + "@" + ('b' + i) + ".com",
+//                    "" + ('a' + i) + ('b' + i) + "street",
+//                    "" + i,
+//                    "" + ('a' + i) + ('b' + i) + ('c' + i)));
+//            if(i < 50) {
+//                tables.add(new RoundTable());
+//            }else{
+//                tables.add(new SquareTable());
+//            }
+//            extras.add(new Extra(""+i, i*(new Random().nextInt(5))));
+//            ArrayList<Extra> tempExtras = new ArrayList<>();tempExtras.add(extras.get(i));
+//            Address address = new Address("" + 'a' + i,
+//                    i,
+//                    "" + ('a' + i) + "@" + ('b' + i) + ".com",
+//                    "" + ('a' + i) + ('b' + i) + "street",
+//                    "" + i);
+//            Supplier supplier = new Supplier("" + ('a' + i),
+//                    address,
+//                    ""+i + "" + (i+1) + "" + (i+2),
+//                    ""+ i + "" + (i+1) + "" + (i+2));
+//            ingredients.add(new Ingredient(""+('a' + i),"gram", i,i, supplier, "" + ('c' + i)));
+//
+//            menuItems.add(new MenuItem(ingredients));
+//            orders.add(new Order(i,menuItems,i,"in progress", new Date()));
+//            bookings.add(new Booking(guests.get(i),i,new Date(),new Random().nextInt(6),tempExtras, orders));
+//        }
+//    }
+//
+//    public ArrayList<Guest> getGuests() {
+//        return guests;
+//    }
+//
+//    public ArrayList<Booking> getBookings() {
+//        return bookings;
+//    }
+//
+//    public ArrayList<Table> getTables() {
+//        return tables;
+//    }
+//
+//    public ArrayList<Order> getOrders() {
+//        return orders;
+//    }
+//
+//    public ArrayList<MenuItem> getMenuItems() {
+//        return menuItems;
+//    }
+//
+//    public ArrayList<Ingredient> getIngredients() {
+//        return ingredients;
+//    }
+//
+//    public ArrayList<Extra> getExtras() {
+//        return extras;
+//    }
+//}
--- src/Tests/TableTest.java
+++ src/Tests/TableTest.java
@@ -1,12 +1,8 @@
 package Tests;
 
-import Models.RoundTable;
-import Models.SquareTable;
-import Models.Table;
-import Models.TableStatus;
+import Models.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,34 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TableTest {
 
-    private List<Table> squareTables = new ArrayList<Table>();
-    private List<Table> roundTables = new ArrayList<Table>();
+    //todo:
+    //make a list of table that works with tabletype.
+    private List<Table> tables = new ArrayList<Table>();
     private int tablesListSize;
-    private TableStatus available, reserved, tooLate;
+    private TableType round, square;
 
     @BeforeEach
     void setUp() {
         tablesListSize = 10;
         for(int i = 0; i < tablesListSize; i++){
-                squareTables.add(new SquareTable(i+1));
-                roundTables.add(new RoundTable(i+11));
+                tables.add(new Table(i+1,TableType.ROUND));
         }
-
-        available = TableStatus.AVAILABLE;
-        reserved = TableStatus.RESERVED;
-        tooLate = TableStatus.TOO_LATE;
+        round = TableType.ROUND;
+        square = TableType.SQUARE;
     }
 
     @Test
     void getNumber() {
-        for(int i = 0; i < squareTables.size(); i++){
-            assertEquals(i+1, squareTables.get(i).getNumber());
+        for(int i = 0; i < tables.size(); i++){
+            assertEquals(i+1, tables.get(i).getNumber());
         }
     }
 
     @Test
     void setNumber() {
-        Table t1 = new SquareTable(1);
+        Table t1 = new Table(1,TableType.ROUND);
         t1.setNumber(5);
         assertEquals(5, t1.getNumber());
         assertNotEquals(1, t1.getNumber());
@@ -50,31 +44,23 @@ class TableTest {
     @Test
     void getCapacity() {
         for(int i = 0; i < tablesListSize; i++){
-            assertEquals(4, squareTables.get(i).getCapacity());
-            assertEquals(12, roundTables.get(i).getCapacity());
+            assertEquals(12, tables.get(i).getCapacity());
         }
     }
 
     @Test
     void setCapacity() {
-        Table t1 = new SquareTable(0);
-        t1.setCapacity(6);
-        assertEquals(6, t1.getCapacity());
-        assertNotEquals(4, t1.getCapacity());
+        Table t1 = new Table(4 , TableType.ROUND);
+        t1.setCapacity(4);
+        assertEquals(4, t1.getCapacity());
+        assertNotEquals(6, t1.getCapacity());
     }
 
     @Test
     void getStatus() {
-        //A table is initialized with AVAILABLE.
-        assertEquals(available, squareTables.get(0).getStatus());
-        assertNotEquals(reserved, squareTables.get(0).getStatus());
-        assertNotEquals(tooLate, squareTables.get(0).getStatus());
+        //A table is initialized with round.
+        assertEquals(round, tables.get(0).getType());
+        assertNotEquals(square, tables.get(0).getType());
     }
 
-    @Test
-    void changeStatus() {
-        assertEquals(available, squareTables.get(0).getStatus());
-        squareTables.get(0).changeStatus(TableStatus.RESERVED);
-        assertEquals(reserved, squareTables.get(0).getStatus());
-    }
 }
