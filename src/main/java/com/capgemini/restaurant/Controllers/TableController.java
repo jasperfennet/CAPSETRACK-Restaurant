eea60f4b636bba645cdff6f2316eb5ceb8a7ef31
Jasper Fennet <jasper.fennet@gmail.com>
2018-09-27 15:41:45 +0200

    Added html for demo, styling, mostly everything for the demo

--- src/main/java/com/capgemini/restaurant/Controllers/TableController.java
+++ src/main/java/com/capgemini/restaurant/Controllers/TableController.java
@@ -8,36 +8,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
+
 @RequestMapping("/api/tables")
 public class TableController {
 
     @Autowired
     private TableRepository tableRepository;
 
+    @CrossOrigin
     @GetMapping("/list")
     public Collection<Table> list() {
         return tableRepository.findAll();
     }
 
+    @CrossOrigin
     @GetMapping("/get/{tableNR}")
     public Table findByTableNR(@PathVariable int tableNR) {
         return tableRepository.findByTableNR(tableNR);
     }
 
-
+    @CrossOrigin
     @PostMapping("/post")
     public Table addTable(@RequestBody Table newTable) {
         this.tableRepository.save(newTable);
         return newTable;
     }
 
+    @CrossOrigin
     @DeleteMapping("/delete/{tableNR}")
-    public void deleteByTableNR(@PathVariable int tableNR){
+    public void deleteByTableNR(@PathVariable int tableNR) {
         this.tableRepository.deleteByTableNR(tableNR);
     }
 
+    @CrossOrigin
     @PutMapping("update/{tableNR}")
-    public Table updateByTableNR(@PathVariable int tableNR, @RequestBody Table update){
+    public Table updateByTableNR(@PathVariable int tableNR, @RequestBody Table update) {
         return this.tableRepository.updateTable(tableNR, update);
     }
 }
