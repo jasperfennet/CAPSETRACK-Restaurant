fbb88f2a3112a326da52bde399f8faba05f5a3ec
HVANOS <hans.van.os@capgemini.com>
2018-09-28 15:53:54 +0200

    commented out security code for now otherwise js wont load

--- src/main/java/com/capgemini/restaurant/authentication/SecurityControllerExample.java
+++ src/main/java/com/capgemini/restaurant/authentication/SecurityControllerExample.java
@@ -1,16 +1,16 @@
 package com.capgemini.restaurant.authentication;
 
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-//dit kan bij de controllers
-@RestController
-public class SecurityControllerExample {
-
-//    //ROLE_ prefix is verplicht
-//    @Secured("ROLE_EMPLOYEE")
-//    @RequestMapping("/")
-//    //etc etc
-}
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+////dit kan bij de controllers
+//@RestController
+//public class SecurityControllerExample {
+//
+////    //ROLE_ prefix is verplicht
+////    @Secured("ROLE_EMPLOYEE")
+////    @RequestMapping("/")
+////    //etc etc
+//}
--- src/main/java/com/capgemini/restaurant/authentication/UserDetailServiceImpl.java
+++ src/main/java/com/capgemini/restaurant/authentication/UserDetailServiceImpl.java
@@ -1,26 +1,26 @@
 package com.capgemini.restaurant.authentication;
 
-import com.capgemini.restaurant.Models.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public class UserDetailServiceImpl implements UserDetailsService {
-
-//    @Autowired
-//    private PersonRepository personRepository;
-
-
-//    //zowel guest als employee gemaakt worden
-//    @Override
-    public User loadUserByUsername(String username) {
-//        Employee employee = personRepository.getSinglePerson(username);
-//        //to be made
-//        return mapPersonToUser(person);
-        return null;
-    }
-//    //Arrays.asList(new SimpleGrantedAuthority()) voegt rollen toe.
-//    private User mapPersonToUser(Person person){
-//        return new User(person.getMail(), person.getPassword(), Arrays.asList(new SimpleGrantedAuthority(person.getRole())));
+//import com.capgemini.restaurant.Models.Employee;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//public class UserDetailServiceImpl implements UserDetailsService {
+//
+////    @Autowired
+////    private PersonRepository personRepository;
+//
+//
+////    //zowel guest als employee gemaakt worden
+////    @Override
+//    public User loadUserByUsername(String username) {
+////        Employee employee = personRepository.getSinglePerson(username);
+////        //to be made
+////        return mapPersonToUser(person);
+//        return null;
 //    }
-}
+////    //Arrays.asList(new SimpleGrantedAuthority()) voegt rollen toe.
+////    private User mapPersonToUser(Person person){
+////        return new User(person.getMail(), person.getPassword(), Arrays.asList(new SimpleGrantedAuthority(person.getRole())));
+////    }
+//}
