693a1a703e7e52aa20af03a1465e4360046ddc9d
Hagen <jeroen.hagen@capgemini.com>
2018-10-04 14:04:03 +0200

    Unit test Ingredient updated with new fields

--- src/test/java/com/capgemini/restaurant/IngredientTest.java
+++ src/test/java/com/capgemini/restaurant/IngredientTest.java
@@ -4,93 +4,94 @@ import com.capgemini.restaurant.Models.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IngredientTest {
-  private Ingredient ingredient;
-  private Supplier supplier;
+  private List<Ingredient> ingredients;
+  private List<Supplier> suppliers;
+  private List<Allergy> allergies;
 
   @BeforeEach
   public void setUp() throws Exception {
-    supplier =
-        new Supplier(
+    suppliers.add(new Supplier(
             "Meat Delivery",
             new Address("Streetname", 123, "", "541000", "Wuhan", "11", "China"),
-            "+54", "0612354678","NL12BANK1234567810", "mail@example.com");
-    Allergy strawberryFever = new Allergy("Strawberry Fever");
-    ingredient = new Ingredient("Strawberry's", "Kilo", 10, 20.00, supplier, strawberryFever);
+            "+54", "0612354678","NL12BANK1234567810", "mail@example.com"));
+    allergies.add(new Allergy("Strawberry Fever"));
+    ingredients.add(new Ingredient("Strawberry's", "Kilo", 10, 20.00, suppliers, allergies));
   }
 
   @Test
   public void getName() {
-    assertEquals("Strawberry's", ingredient.getName());
+    assertEquals("Strawberry's", ingredients.get(0).getName());
   }
 
   @Test
   public void setName() {
-    ingredient.setName("Blue Strawberry's");
-    assertEquals("Blue Strawberry's", ingredient.getName());
+    ingredients.get(0).setName("Blue Strawberry's");
+    assertEquals("Blue Strawberry's", ingredients.get(0).getName());
   }
 
   @Test
   public void getUnit() {
-    assertEquals("Kilo", ingredient.getUnit());
+    assertEquals("Kilo", ingredients.get(0).getUnit());
   }
 
   @Test
   public void setUnit() {
-    ingredient.setUnit("Liter");
-    assertEquals("Liter", ingredient.getUnit());
+    ingredients.get(0).setUnit("Liter");
+    assertEquals("Liter", ingredients.get(0).getUnit());
   }
 
   @Test
   public void getAmount() {
-    assertEquals(10, ingredient.getAmount());
+    assertEquals(10, ingredients.get(0).getAmount());
   }
 
   @Test
   public void setAmount() {
-    ingredient.setAmount(15);
-    assertEquals(15, ingredient.getAmount());
+    ingredients.get(0).setAmount(15);
+    assertEquals(15, ingredients.get(0).getAmount());
   }
 
   @Test
   public void getPrice() {
-    assertEquals(20.00, ingredient.getPrice());
+    assertEquals(20.00, ingredients.get(0).getPrice());
   }
 
   @Test
   public void setPrice() {
-    ingredient.setPrice(21.00);
-    assertEquals(21.00, ingredient.getPrice());
+    ingredients.get(0).setPrice(21.00);
+    assertEquals(21.00, ingredients.get(0).getPrice());
   }
 
   @Test
   public void getSupplier() {
-    assertEquals(supplier, ingredient.getSupplier());
+    assertEquals(suppliers, ingredients.get(0).getSuppliers());
   }
 
   @Test
   public void setSupplier() {
-    Supplier supplier2 =
-        new Supplier(
+    suppliers.add(new Supplier(
             "Meat Delivery",
             new Address("Streetname", 123, "", "541000", "Wuhan", "11", "China"),
-            "+54", "0612354678", "NL12BANK1234567810", "mail@example.com");
+            "+54", "0612354678", "NL12BANK1234567810", "mail@example.com"));
 
-    ingredient.setSupplier(supplier2);
-    assertEquals(supplier2, ingredient.getSupplier());
+    ingredients.get(0).setSuppliers(suppliers);
+    assertEquals(suppliers, ingredients.get(0).getSuppliers());
   }
 
   @Test
   public void getAllergy() {
-    assertEquals("Strawberry Fever", ingredient.getAllergy());
+    assertEquals("Strawberry Fever", ingredients.get(0).getAllergies());
   }
 
   @Test
   public void setAllergy() {
-    Allergy blueStrawberryFever = new Allergy("Blue Strawberry Fever");
-    ingredient.setAllergy(blueStrawberryFever);
-    assertEquals(blueStrawberryFever, ingredient.getAllergy());
+    allergies.add(new Allergy("Milk"));
+    ingredients.get(0).setAllergies(allergies);
+    assertEquals(allergies, ingredients.get(0).getAllergies());
   }
 }
